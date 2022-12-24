d56f393ac2d810b0f55cc8ae93245fca6f8e9690
MARCO <mrconietoprez@gmail.com>
2022-12-24 22:34:55 +0100

    Updated main page and changed display of github repos

--- indexscript.js
+++ indexscript.js
@@ -1,13 +1,22 @@
+var lstats = "https://github-readme-stats.vercel.app/api/top-langs/?username=Mnietoprez&langs_count=6&layout=compact&theme=graywhite";
 var lrepo1 = "https://github-readme-stats.vercel.app/api/pin/?username=Mnietoprez&repo=hardy-weinberg-theorem&theme=graywhite";
-var lrepo2 = "https://github-readme-stats.vercel.app/api/pin/?username=Mnietoprez&repo=newton-fractal-deep-learning&theme=graywhite";
+var lrepo2 = "https://github-readme-stats.vercel.app/api/pin/?username=Mnietoprez&repo=web_smart_contracts&theme=graywhite";
+var lrepo3 = "https://github-readme-stats.vercel.app/api/pin/?username=Mnietoprez&repo=newton-fractal-deep-learning&theme=graywhite";
+var lrepo4 = "https://github-readme-stats.vercel.app/api/pin/?username=Mnietoprez&repo=hardy-weinberg-theorem&theme=graywhite";
+var dstats = "https://github-readme-stats.vercel.app/api/top-langs/?username=Mnietoprez&langs_count=6&layout=compact&theme=dark";
 var drepo1 = "https://github-readme-stats.vercel.app/api/pin/?username=Mnietoprez&repo=hardy-weinberg-theorem&theme=dark";
-var drepo2 = "https://github-readme-stats.vercel.app/api/pin/?username=Mnietoprez&repo=newton-fractal-deep-learning&theme=dark";
+var drepo2 = "https://github-readme-stats.vercel.app/api/pin/?username=Mnietoprez&repo=web_smart_contracts&theme=dark";
+var drepo3 = "https://github-readme-stats.vercel.app/api/pin/?username=Mnietoprez&repo=newton-fractal-deep-learning&theme=dark";
+var drepo4 = "https://github-readme-stats.vercel.app/api/pin/?username=Mnietoprez&repo=hardy-weinberg-theorem&theme=dark";
 var light;
 
 window.onload = function() {
     light = false;
     document.getElementById("idr1").src = drepo1;
     document.getElementById("idr2").src = drepo2;
+    document.getElementById("idr3").src = drepo3;
+    document.getElementById("idr4").src = drepo4;
+    document.getElementById("ghstats").src = dstats;
 };
 
 function darkLightToggle() {
@@ -17,6 +26,9 @@ function darkLightToggle() {
         light = false;
         document.getElementById("idr1").src = drepo1;
         document.getElementById("idr2").src = drepo2;
+        document.getElementById("idr3").src = drepo3;
+        document.getElementById("idr4").src = drepo4;
+        document.getElementById("ghstats").src = dstats;
     }
     else{
         document.body.style.transition = 'background 3s ease-in-out';
@@ -24,6 +36,9 @@ function darkLightToggle() {
         light = true
         document.getElementById("idr1").src = lrepo1;
         document.getElementById("idr2").src = lrepo2;
+        document.getElementById("idr3").src = lrepo3;
+        document.getElementById("idr4").src = lrepo4;
+        document.getElementById("ghstats").src = lstats;
     }   
 }
 
