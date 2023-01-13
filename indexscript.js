2b2f4620060478965663d6ef609ee1b4a039a450
M NIETO <mrconietoprez@gmail.com>
2023-01-13 13:10:36 +0100

    Finished language displayer

--- indexscript.js
+++ indexscript.js
@@ -1,22 +1,7 @@
-var lstats = "https://github-readme-stats.vercel.app/api/top-langs/?username=Mnietoprez&langs_count=6&layout=compact&theme=graywhite";
-var lrepo1 = "https://github-readme-stats.vercel.app/api/pin/?username=Mnietoprez&repo=cryptoCards&theme=graywhite";
-var lrepo2 = "https://github-readme-stats.vercel.app/api/pin/?username=Mnietoprez&repo=web_smart_contracts&theme=graywhite";
-var lrepo3 = "https://github-readme-stats.vercel.app/api/pin/?username=Mnietoprez&repo=newton-fractal-deep-learning&theme=graywhite";
-var lrepo4 = "https://github-readme-stats.vercel.app/api/pin/?username=Mnietoprez&repo=hardy-weinberg-theorem&theme=graywhite";
-var dstats = "https://github-readme-stats.vercel.app/api/top-langs/?username=Mnietoprez&langs_count=6&layout=compact&theme=dark";
-var drepo1 = "https://github-readme-stats.vercel.app/api/pin/?username=Mnietoprez&repo=cryptoCards&theme=dark";
-var drepo2 = "https://github-readme-stats.vercel.app/api/pin/?username=Mnietoprez&repo=web_smart_contracts&theme=dark";
-var drepo3 = "https://github-readme-stats.vercel.app/api/pin/?username=Mnietoprez&repo=newton-fractal-deep-learning&theme=dark";
-var drepo4 = "https://github-readme-stats.vercel.app/api/pin/?username=Mnietoprez&repo=hardy-weinberg-theorem&theme=dark";
 var light;
 
 window.onload = function() {
     light = false;
-    document.getElementById("idr1").src = drepo1;
-    document.getElementById("idr2").src = drepo2;
-    document.getElementById("idr3").src = drepo3;
-    document.getElementById("idr4").src = drepo4;
-    document.getElementById("ghstats").src = dstats;
 };
 
 function darkLightToggle() {
@@ -24,21 +9,11 @@ function darkLightToggle() {
         document.body.style.transition = 'background 3s ease-in-out';
         document.body.style.background = `linear-gradient(to right, Black, LightSlateGray)`;
         light = false;
-        document.getElementById("idr1").src = drepo1;
-        document.getElementById("idr2").src = drepo2;
-        document.getElementById("idr3").src = drepo3;
-        document.getElementById("idr4").src = drepo4;
-        document.getElementById("ghstats").src = dstats;
     }
     else{
         document.body.style.transition = 'background 3s ease-in-out';
         document.body.style.background = `linear-gradient(to right, mediumslateblue, Lightblue)`;
         light = true
-        document.getElementById("idr1").src = lrepo1;
-        document.getElementById("idr2").src = lrepo2;
-        document.getElementById("idr3").src = lrepo3;
-        document.getElementById("idr4").src = lrepo4;
-        document.getElementById("ghstats").src = lstats;
     }   
 }
 
@@ -51,9 +26,45 @@ function openP2(){
 }   
 
 function enciende1(){
-    document.imglang1.opacity= "100%" ;
+    document.getElementsByClassName('langpreview')[0].style.background = "linear-gradient(to right, rgba(0, 109, 252, 0.4), rgba(166, 0, 255, 0.4))";
+    document.getElementById('presentation').textContent = "Python";
+    document.getElementById('description').textContent = "High level language, used mostly for science and statistics. Experience in libraries such as 'Matplotlib, NumPy, SymPy, Pandas, Random, among others.";
+    document.getElementById('laimagen').src = "images/python.png";
 }   
 
-function apaga1(){
-    document.imglang1.opacity = "0%" ;
+function enciende2(){
+    document.getElementsByClassName('langpreview')[0].style.background = "linear-gradient(to right, rgba(136, 73, 0, 0.4), rgba(255, 60, 0, 0.4))";
+    document.getElementById('presentation').textContent = "Solidity";
+    document.getElementById('description').textContent = "High level language used for Ethereum Virtual Machine based smart contracts. Experience in the developement environment 'Hardhat'.";
+    document.getElementById('laimagen').src = "images/solidity.png";
 }   
+
+function enciende3(){
+    document.getElementsByClassName('langpreview')[0].style.background = "linear-gradient(to right, rgb(213, 202, 0), rgba(255, 174, 0, 0.4))";
+    document.getElementById('presentation').textContent = "Javascript";
+    document.getElementById('description').textContent = "Powerful language used for web apps. Experience in libraries such as web3.js, used to access Blockcahain smart contracts from a frontend display.";
+    document.getElementById('laimagen').src = "images/js.png";
+}   
+
+function enciende4(){
+    document.getElementsByClassName('langpreview')[0].style.background = "linear-gradient(to right, rgb(51, 255, 0, 0.4), rgba(162, 255, 0, 0.4))";
+    document.getElementById('presentation').textContent = "LaTeX";
+    document.getElementById('description').textContent = "A well-known language in sciences, used to write high quality articles and introduce mathematical formulation easily.";
+    document.getElementById('laimagen').src = "images/latex.png";
+}   
+
+function enciende5(){
+    document.getElementsByClassName('langpreview')[0].style.background = "linear-gradient(to right, rgb(255, 0, 204, 0.4), rgba(253, 0, 84, 0.4))";
+    document.getElementById('presentation').textContent = "CSS/HTML";
+    document.getElementById('description').textContent = "Powerful tools for web designing and structuring, crucial for making user friendly apps.";
+    document.getElementById('laimagen').src = "images/csshtml.png";
+}   
+
+var originalContent = 'Find all my repositories at <a href=\'https://github.com/Mnietoprez?tab=repositories\'>GitHub</a>';
+
+function apaga(){
+    document.getElementsByClassName('langpreview')[0].style.background = "rgba(0, 0, 0, 0.3)";
+    document.getElementById('presentation').innerHTML = originalContent;
+    document.getElementById('description').textContent = "Hover over the words at the left to check details.";
+    document.getElementById('laimagen').src = "images/ghub.png";
+}  
