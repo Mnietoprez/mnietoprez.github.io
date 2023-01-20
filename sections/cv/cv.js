ceb396f18c69f8b684fee67ebd0a52a19c0955ff
M NIETO <mrconietoprez@gmail.com>
2023-01-20 01:59:55 +0100

    Updated cv section

--- sections/cv/cv.js
+++ sections/cv/cv.js
@@ -6,13 +6,24 @@ window.onload = function() {
 
 function darkLightToggle() {
     if (light){
-        document.body.style.transition = 'background 3s ease-in-out';
-        document.body.style.background = `linear-gradient(to right, Black, LightSlateGray)`;
+        document.body.style.background = `linear-gradient(to bottom, black, rgb(42, 42, 42))`;
+        document.querySelectorAll("h1").forEach(e => e.style.color = 'white');
+        document.querySelectorAll("a").forEach(e => e.style.color = 'white');
+        document.querySelectorAll("p").forEach(e => e.style.color = 'white');
+        document.querySelectorAll("h2").forEach(e => e.style.color = 'white');
+        document.getElementsByClassName("maincontainer")[0].style.background = 'rgba(0, 0, 0, 0.3)';
         light = false;
     }
     else{
-        document.body.style.transition = 'background 3s ease-in-out';
-        document.body.style.background = `linear-gradient(to right, mediumslateblue, Lightblue)`;
+        document.body.style.background = `linear-gradient(to bottom, rgb(204, 208, 255), rgb(255, 255, 255))`;
+        document.querySelectorAll("h1").forEach(e => e.style.color = 'black');
+        document.querySelectorAll("a").forEach(e => e.style.color = 'black');
+        document.querySelectorAll("p").forEach(e => e.style.color = 'black');
+        document.querySelectorAll("h2").forEach(e => e.style.color = 'black');
+        document.getElementsByClassName("maincontainer")[0].style.background = 'rgba(103, 100, 127, 0.463)';
+        
+        //Keeps topnav elements white colored
+        document.getElementById("topnav").querySelectorAll("a").forEach(e => e.style.color = 'white');
         light = true
     }   
 }
