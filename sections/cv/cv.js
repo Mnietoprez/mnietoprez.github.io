f876a64030fff939a0f3cd2fad5d252990cab98b
M NIETO <mrconietoprez@gmail.com>
2023-01-23 13:48:41 +0100

    Fixed a main page bug and modified cv section

--- sections/cv/cv.js
+++ sections/cv/cv.js
@@ -2,7 +2,53 @@ var light;
 
 window.onload = function() {
     light = false;
-};
+
+    // Select all slides
+    slides = document.querySelectorAll(".slide");
+  
+    // loop through slides and set each slides translateX property to index * 100% 
+    slides.forEach((slide, indx) => {
+      slide.style.transform = `translateX(${indx * 100}%)`;
+    });
+  
+    // current slide counter
+    curSlide = 0;
+  
+    // maximum number of slides
+    maxSlide = slides.length - 1;
+  };
+  
+  
+  function nextsl() {
+      // check if current slide is the last and reset current slide
+      if (curSlide === maxSlide) {
+          curSlide = 0;
+      } else {
+          curSlide++;
+      }
+  
+      //move slide by -100%
+      slides.forEach((slide, indx) => {
+      slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+      });
+  };
+  
+  
+  function prevsl() {
+    // check if current slide is the first and reset current slide to last
+    if (curSlide === 0) {
+      curSlide = maxSlide;
+    } else {
+      curSlide--;
+    }
+  
+    //   move slide by 100%
+    slides.forEach((slide, indx) => {
+      slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+    });
+  };
+  
+
 
 function darkLightToggle() {
     if (light){
